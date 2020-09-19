5d6aa93fe62bc4b518a36aab2a7ea2c4cd387581
Larry Cessna <larrybcessna@gmail.com>
2020-09-19 08:09:27 -0700

    Day rows are rendering to the page.

--- README.md
+++ README.md
@@ -3,18 +3,18 @@
 - Moved index.html and style.css to folder.
 - Accidentally added a package-lock.json. Not sure what that is; moving on.
 - Added getDate function and appended to the header.
-
+- Still working on getting colors to show. However, the times are there.
 
 
 
 ## Acceptance Criteria
 
-```
+
 GIVEN I am using a daily planner to create a schedule
-- [x] WHEN I open the planner THEN the current day is displayed at the top of the calendar
-- [ ] WHEN I scroll down THEN I am presented with timeblocks for standard business hours
+- [X] WHEN I open the planner THEN the current day is displayed at the top of the calendar
+- [X] WHEN I scroll down THEN I am presented with timeblocks for standard business hours
 - [ ] WHEN I view the timeblocks for that day THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 - [ ] WHEN I click into a timeblock THEN I can enter an event
 - [ ] WHEN I click the save button for that timeblock THEN the text for that event is saved in local storage
 - [ ] WHEN I refresh the page THEN the saved events persist
-```
+
--- script.js
+++ script.js
@@ -7,3 +7,90 @@ function getDate() {
 }
 getDate();
 
+// Array of objects. Each id represents one hour.
+var workDay = [
+    {
+        id: "0",
+        hour: "09",
+        time: "09",
+        amPm: "am",
+        reminder: ""
+    },
+    {
+        id: "1",
+        hour: "10",
+        time: "10",
+        amPm: "am",
+        reminder: ""
+    },
+    {
+        id: "2",
+        hour: "11",
+        time: "11",
+        amPm: "am",
+        reminder: ""
+    },
+    {
+        id: "3",
+        hour: "12",
+        time: "12",
+        amPm: "pm",
+        reminder: ""
+    },
+    {
+        id: "4",
+        hour: "01",
+        time: "13",
+        amPm: "pm",
+        reminder: ""
+    },
+    {
+        id: "5",
+        hour: "02",
+        time: "14",
+        amPm: "pm",
+        reminder: ""
+    },
+    {
+        id: "6",
+        hour: "03",
+        time: "15",
+        amPm: "pm",
+        reminder: ""
+    },
+    {
+        id: "7",
+        hour: "04",
+        time: "16",
+        amPm: "pm",
+        reminder: ""
+    },
+    {
+        id: "8",
+        hour: "05",
+        time: "17",
+        amPm: "pm",
+        reminder: ""
+    }]
+
+// Create rows for each time block in workDay
+workDay.forEach(function (thisHour) {
+    var hourRow = $("<form>").attr({ "class": "row" });
+    $(".container").append(hourRow);
+
+    var hourField = $("<div>").text(`${thisHour.hour + " "}${thisHour.amPm}`).attr({ "class": "col-md-2 hour" });
+
+    var hourPlan = $("<div>").attr({ "class": "col-md-9 description p-0" });
+    var planData = $("<textarea>");
+    hourPlan.append(planData);
+    planData.attr("id", thisHour.id);
+    if (thisHour.time < moment().format("HH")) {
+        planData.attr({ "class": "past", })
+    } else if (thisHour.time === moment().format("HH")) {
+        planData.attr({ "class": "present" })
+    } else if (thisHour.time > moment().format("HH")) {
+        planData.attr({ "class": "future" })
+    }
+    hourRow.append(hourField);
+});
+
